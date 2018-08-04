482e5252e7d2ee5dcaf8ba1350dc9397fb420183
Annie Zhang <a.y.zhang@hotmail.com>
2018-08-05 03:57:42 +1000

    main page

--- vue/src/components/Index.vue
+++ vue/src/components/Index.vue
@@ -3,10 +3,30 @@
 		<div v-if='!settingsPage'>
 			<div class="main">
 				<div class="svg-action">
-					<img src="../assets/moon.svg" class="svg-action"/>
+					<!-- <img :src="'../assets/' + svg_selected + '.svg'" class="svg-action"/> -->
+					<div v-if='action_idx === 0'>
+						<img src="../assets/moon.svg" class="svg-action"/>
+					</div>
+					<div v-if='action_idx === 1'>
+						<img src="../assets/socks.svg" class="svg-action"/>
+					</div>
+					<div v-if='action_idx === 2'>
+						<img src="../assets/train.svg" class="svg-action"/>
+					</div>
+					<div v-if='action_idx === 3'>
+						<img src="../assets/sheep.svg" class="svg-action"/>
+					</div>
+					<!-- <img v-bind:src="`../assets/${svg_selected}.svg`"/> -->
+					<!-- <img :src="'../assets/'+svg_selected" /> -->
 				</div>
-				<div class="countdown-timer">
-					{{h}}:{{m}}
+				<!-- <div v-if='arrived'>
+					You've arrived!
+				</div> -->
+				<div v-if='!arrived'>
+					<div class="countdown-timer">
+						{{h}}:{{m}} to <br />
+						{{ current_action }}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -46,7 +66,7 @@
 <style lang='scss' src='@/assets/main.scss'></style>
 
 <script>
-const au = new Audio('@/assets/He-man.mp3');
+const au = new Audio("@/assets/He-man.mp3");
 au.play();
 const thingos = {
   day: [
@@ -101,6 +121,21 @@ const thingos = {
   ]
 };
 
+const all_actions = [
+  "sleep",
+  "wake up",
+  "train arrives",
+  "arriving at Central station"
+];
+const all_durations = [12, 15, 15, 15];
+let action_idx = 0;
+
+function secToMins(secs) {
+	  return secs - secToHours(secs) * 60;
+  }
+  function secToHours(secs) {
+	  return Math.floor(secs / 60);
+  }
 export default {
   data() {
     return {
@@ -108,40 +143,53 @@ export default {
       currentAction: "day",
       typeOfDay: "",
       startingTime: "",
-	  travelMethod: "",
-	  settingsPage: false,
-	  hours: 1,
-	  minutes: 25,
-	  dte: undefined
+      travelMethod: "",
+      settingsPage: false,
+      hours: secToHours(all_durations[0]),
+	  minutes: secToMins(all_durations[0]),
+	  action_idx: 0,
+      dte: 0,
+	  current_action: all_actions[0],
+	  arrived: false
     };
   },
   mounted() {
-	  const sec = 100;
-	  this.dte = new Date();
-	  setInterval(() => {
-		  this.dte = new Date(this.dte.setMinutes(this.dte.getMinutes() - 1));
-		  this.hours = this.dte.getHours();
-		  this.minutes = this.dte.getMinutes();
-	  }, sec);
+    const sec = 100;
+    this.dte = this.hours * 60 + this.minutes;
+    setInterval(() => {
+	  this.dte -= 1; //new Date(this.dte.setMinutes(this.dte.getMinutes() - 1));
+	//   if (this.dte === 0 && this.action_idx === all_actions.length) {
+	// 	  this.arrived = true;
+	//   }
+      if (this.dte === 0) {
+		action_idx += 1;
+		this.current_action = all_actions[action_idx];
+		this.dte = all_durations[action_idx];
+		this.hours = secToHours(all_durations[action_idx]);
+		this.minutes = secToMins(all_durations[action_idx]);
+	  }
+      this.hours = secToHours(this.dte); //this.dte.getMinutes();
+      this.minutes = secToMins(this.dte); //this.dte.getHours();
+    }, sec);
   },
   computed: {
-	  m: function() {
-		  var s = String('0' + this.minutes).slice(-2);
-		  return s > 0 ? s : '00';
-	  },
-	  h: function() {
-		  var s = String('0' + this.hours).slice(-2);
-		  return s > 0 ? s : '00';
-	  }
+    m: function() {
+      var s = String("0" + this.minutes).slice(-2);
+      return s > 0 ? s : "00";
+    },
+    h: function() {
+      var s = String("0" + this.hours).slice(-2);
+      return s > 0 ? s : "00";
+    }
   },
   methods: {
-	changePage() {
-		if (this.settingsPage) {
-			this.settingsPage = false;
-		} else {
-			this.settingsPage = true;
-		}
-	},
+    changePage() {
+      if (this.settingsPage) {
+        this.settingsPage = false;
+      } else {
+        this.settingsPage = true;
+      }
+    },
     setTypeOfDay(dayType) {
       this.typeOfDay = dayType;
       this.currentAction = "time";
