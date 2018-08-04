86cdf9b281b15b390b131ee4ee1cedd8c94185e3
Harry Park <harry@harrypark.io>
2018-08-04 16:47:12 +1000

    fix server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,30 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 
+import chalk from 'chalk';
 import * as bodyParser from 'koa-bodyparser';
+
+import * as constants from './constants';
 
 const app = new Koa();
 const router = new Router();
 
-import { Index } from '@/routes/index';
+import { Index } from './routes/index';
 
-router.use('/', Index.routes);
+router.use('', Index.routes);
 
 app.use(async (ctx, next) => {
 	try {
 		await next();
 
-		if (ctx.status == 404) {
+		if (ctx.status === 404) {
 			ctx.throw(404);
 		}
 	} catch (err) {
 		ctx.status = err.status;
 		ctx.body = {
+			error: true,
+			status_code: ctx.status,
 			message: err.message
 		};
 	}
@@ -28,3 +33,7 @@ app.use(async (ctx, next) => {
 app
 	.use(router.routes())
 	.use(router.allowedMethods());
+
+app.listen(constants.port, () => {
+	console.log(`\nRunning server on localhost:${chalk.red(constants.port.toString())}`);
+});
